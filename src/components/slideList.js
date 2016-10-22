312bb79f33b2b1b247929171edbd34980b31ef5c
ALI1995 <ali1364137942@outlook.com>
2016-10-22 13:02:57 +0800

    初步完成页面循环轮播效果

--- src/components/slideList.js
+++ src/components/slideList.js
@@ -11,14 +11,21 @@ class SlideList extends React.Component{
         this.state = {
             curIndex: 0
         };
-        this.count = Images.length;
+        this.count = Images.length + 1;
+        this.transition = "all 0.8s linear";
     }
     turn(n){
         let _n = this.state.curIndex + n;
+        if(_n == this.count){
+            this.transition = "none";
+        }else{
+            this.transition = "all 0.8s linear";
+        }
         if(_n < 0){
             _n = _n + this.count;
         }
-        if(_n > this.count){
+
+        if(_n >= this.count){
             _n = _n - this.count;
         }
         this.setState({curIndex: _n});
@@ -39,9 +46,10 @@ class SlideList extends React.Component{
         Images.forEach((item, i) => {
            ImageLi.push(<SlideItem key={i} item={item} count={this.count}/>);
         });
-        let width = this.props.width * this.count;
+        ImageLi.push(<SlideItem key={this.count - 1} item={Images[0]} count={this.count}/>);
+        let width = this.props.width * (this.count);
         return(
-            <ul id="banner" style={{width: width,position: 'relative',top: 0, left: '-800' * this.state.curIndex + 'px'}}>
+            <ul id="banner" style={{width: width,position: 'relative',top: 0, left: '-800' * this.state.curIndex + 'px', transition: this.transition}}>
                 {ImageLi}
             </ul>
         )
