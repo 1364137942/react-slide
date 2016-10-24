31f37b57acda5a5c5523544a403f8e3041a4c313
ALI1995 <ali1364137942@outlook.com>
2016-10-24 23:40:38 +0800

    add arrow

--- src/components/slideArrow.js
+++ src/components/slideArrow.js
@@ -4,5 +4,26 @@
 "use strict";
 let React = require("react");
 class SlideArrow extends React.Component{
-
+    handleArrowClick(i){
+        this.props.turn(i, "arrow");
+    }
+    render(){
+        return(
+            <div id="slide-arrow-box">
+                <span
+                    className="slide-arrow slide-arrow-left"
+                    onClick={this.handleArrowClick.bind(this, -1)}
+                >
+                    &lt;
+                </span>
+                <span
+                    className="slide-arrow slide-arrow-right"
+                    onClick={this.handleArrowClick.bind(this, 1)}
+                >
+                    &gt;
+                </span>
+            </div>
+        )
+    }
 }
+module.exports = SlideArrow;
--- src/components/slideList.js
+++ src/components/slideList.js
@@ -3,8 +3,9 @@
  */
 let React = require("react");
 let SlideItem = require("./slideItem");
-let Images = require("../readImage/Image.json");
 let SlideDots = require("./slideDots");
+let SlideArrow = require("./slideArrow");
+let Images = require("../readImage/Image.json");
 
 class SlideList extends React.Component{
     constructor(props){
@@ -16,13 +17,25 @@ class SlideList extends React.Component{
         this.transition = "all 0.8s linear";
         this.time = null;
     }
-    turn(n, dots){
+    turn(n, type){
         let _n;
-        if(dots){
-            _n = n;
-            this.pause();
-        }else {
-            _n = this.state.curIndex + n;
+        type = type || null;
+        switch (type){
+            case null:
+                _n = this.state.curIndex + n;
+                break;
+            case "dots":
+                _n = n;
+                break;
+            case "arrow":
+                //arrow时需要注意，因为为了要循环播放，所以图片数目会比总数多一，所以需要在第一张和最后一张（实际时同一张图），以及倒数第二张（用户看到的最后一张）限制操作
+                if(((this.state.curIndex === 0 || this.state.curIndex === this.count - 1) && n == -1) || (this.state.curIndex === this.count - 2 && n == 1)){
+                    return;
+                }else{
+                    _n = this.state.curIndex + n;
+                }
+                break;
+            default: return;
         }
         if(_n == this.count){
             this.transition = "none";
@@ -37,14 +50,11 @@ class SlideList extends React.Component{
         }
 
         this.setState({curIndex: _n});
-        if(this.time == null){
-            this.goplay();
-        }
+
     }
     goplay(){
         this.time = setInterval(() => {
             this.turn(1);
-            // console.log(this.state.curIndex);
         },1000);
     }
     pause(){
@@ -56,18 +66,17 @@ class SlideList extends React.Component{
     }
     render(){
         let ImageLi = [];
-        // let count = Images.length;
-        // let left =
         Images.forEach((item, i) => {
            ImageLi.push(<SlideItem key={i} item={item} count={this.count}/>);
         });
         ImageLi.push(<SlideItem key={this.count - 1} item={Images[0]} count={this.count}/>);
         let width = this.props.width * (this.count);
         return(
-            <section id="container">
+            <section id="container" onMouseOver={this.pause.bind(this)} onMouseOut={this.goplay.bind(this)}>
                 <ul id="banner" style={{width: width,position: 'relative',top: 0, left: '-800' * this.state.curIndex + 'px', transition: this.transition}}>
                     {ImageLi}
                 </ul>
+                <SlideArrow turn={this.turn.bind(this)}/>
                 <SlideDots count={this.count - 1} turn={this.turn.bind(this)} nowLocal={this.state.curIndex === (this.count - 1) ? 0 : this.state.curIndex}/>
             </section>
 
